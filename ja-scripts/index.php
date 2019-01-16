07bb55fe84aff26f0926a5b55817ba3e0a6d789e
Tudor Iovita <iovitatudor@gmail.com>
2019-01-16 09:21:32 +0000

    Added project to github

---
+++ ja-scripts/index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="p:domain_verify" content="c3bf7ecf1e6ecd8476d4f60b2606fe43"/>
+    <style media="screen">
+       body{
+        margin: 0;
+        overflow: hidden;
+       }
+       img{
+         width: 100vw;
+         height: 100vh;
+       }
+       p{
+         text-align: center;
+         color: white;
+         margin: 0;
+       }
+       .footer{
+         width: 100%;
+         background-color: black;
+         padding: 20px 0;
+         position: fixed;
+         bottom: 0;
+         display: none;
+       }
+       .imgMob{
+         display: none;
+       }
+       @media only screen and (max-width: 768px){
+          .imgDesk{
+            display: none;
+          }
+          .imgMob{
+            display: block;
+          }
+          .footer{
+            display: block;
+          }
+          img{
+            height: auto;
+            margin-bottom: 70px;
+          }
+          body{
+           margin: 0;
+           overflow: auto;
+          }
+       }
+    </style>
+  </head>
+  <body>
+    <img class="imgDesk" src="site2.jpg" alt="Jullia Alert">
+    <img class="imgMob" src="Site2.png" alt="Jullia Alert">
+    <div class="footer">
+      <p>
+        Showroom Julia Allert is open every day, <br>
+        from 10:00 to 20:00, M.Eminescu, 39 str. Welcome!
+      </p>
+    </div>
+  </body>
+</html>
